2a41637a8966da3137c3a23c35084d96076ec5ba
Bartlomiejgarus <75130323+Bartlomiejgarus@users.noreply.github.com>
2022-01-16 14:27:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 import pygame
-from warcaby.ustawienia import WIDTH, HEIGHT, SQUARE_SIZE, WHITE
-from warcaby.szachownica import Szachownica
+from warcaby.ustawienia import WIDTH, HEIGHT, WielkoscKwadratu
 from warcaby.gra import Gra
 
-FPS = 60
-
+#Okno do wyswietlana obiektow
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+#nazwa okna
 pygame.display.set_caption("Warcaby")
+pygame.init()
 
 
 def dajPozycjeZMyszki(pos):
+    '''
+    daje dana kolumne i rzad na podstawie wartosci pos
+    :param pos: wartosc pos
+    :return:
+    '''
     x, y = pos
-    row = y // SQUARE_SIZE
-    col = x // SQUARE_SIZE
+    row = y // WielkoscKwadratu
+    col = x // WielkoscKwadratu
     return row, col
 
 
 def main():
+    '''
+    Funkcja glowna ktora uruchamia grę warcaby
+    '''
     run = True
     clock = pygame.time.Clock()
     gra = Gra(WIN)
 
+    #dopuki gra dziala dzialaja na niej zdarzenia
     while run:
-        clock.tick(FPS)
-
-        if gra.szachownica.wygral() != None:
-            print(gra.szachownica.wygral())
+        clock.tick(60)
 
         for event in pygame.event.get():
+
+            # jesli wyjdziemy z gdy
             if event.type == pygame.QUIT:
                 run = False
 
+            #jesli klikniemy myszka
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                row, col = dajPozycjeZMyszki(pos)
-                gra.wybierz(row, col)
 
-        gra.update()
+                #jesli klikiemy przycisk inny niz reset
+                if not gra.reset(pos):
+                    row, col = dajPozycjeZMyszki(pos)
+                    gra.wybierz(row, col)
+
+        gra.grajmy()
 
     pygame.quit()
 
